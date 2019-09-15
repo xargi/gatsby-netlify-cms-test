abfad325cafcf249d380c3d8dabd0212d686eb56
Mikael Viitaniemi <mikael.viitaniemi@zc.fi>
2019-09-15 03:36:08 +0300

    deps

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,4 @@
-import CMS from "netlify-cms";
+import CMS from "netlify-cms-app";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import HomePagePreview from "./preview-templates/HomePagePreview";
